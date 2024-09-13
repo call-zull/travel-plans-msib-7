2e67c7760a1a1156c7a037d60b1440f091bf11ca
ijuldi <ijuldi77@gmail.com>
2024-09-13 10:21:14 +0800

    install package auth,enum,debug-bar,log-viewer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('login');
+// });
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
