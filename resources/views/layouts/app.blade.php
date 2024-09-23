bac4499695c6336741703cf59066066ffc25f082
ijuldi <ijuldi77@gmail.com>
2024-09-23 09:03:55 +0800

    refactor views to component and includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    @include('layouts.css')
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    {{-- @stack('scripts') --}}
+    @include('includes.meta')
+    @include('includes.scripts')
+    @stack('scripts')
 </head>
 
 <body>
     <div id="app">
         @include('sweetalert::alert')
-        @include('layouts.navbar')
+        <x-navbar />
 
         <main class="py-4">
             {{$slot}}
